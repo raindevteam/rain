bb073eb7e6a4070a88ff1e3f50bcb81ebd2e2301
Rodolfo Castillo <rvallada@ucsc.edu>
2015-04-22 00:54:36 -0700

    New TDD, module class needs test funcs implemented

---
+++ modules/rainbin/tests/cmdtests.js
@@ -0,0 +1,13 @@
+expect = require('chai').expect;
+Rainbin = require('../index');
+
+describe('Rainbin', function() {
+   describe('commands', function() {
+      describe('pb', function() {
+         it('should respond with "test text"', function() {
+            response = Rainbin.testCommand('pb', 'testing', null);
+            expect(results).to.equal("test text");
+         });
+      });
+   });
+});
