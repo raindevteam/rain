520b9980135800e30dd0245a1af472e6d3bc8196
Rodolfo Castillo <rvallada@ucsc.edu>
2015-04-24 14:39:17 -0700

    Added triggers

--- modules/rainbin/index.js
+++ modules/rainbin/index.js
@@ -1,11 +1,13 @@
 // As an example, modules don't need to be written in coffeescript, one can
 // still use pure javascript.
-commands = require('./listeners/commands')
+commands = require('./listeners/commands');
+triggers = require('./listeners/triggers');
 
 module.exports = function(Module) {
    Rainbin = new Module("Rainbin");
    console.log(Module)
    Rainbin.addCommands(commands);
+   Rainbin.addTriggers(triggers)
    console.log(Rainbin);
    return Rainbin;
 }
