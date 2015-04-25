2590091b277f27fc05f6f9e0f40830edb660d867
Rodolfo Castillo <rvallada@ucsc.edu>
2015-04-24 18:50:00 -0700

    Removed console logs

--- modules/rainbin/index.js
+++ modules/rainbin/index.js
@@ -5,9 +5,7 @@ triggers = require('./listeners/triggers');
 
 module.exports = function(Module) {
    Rainbin = new Module("Rainbin");
-   console.log(Module)
    Rainbin.addCommands(commands);
    Rainbin.addTriggers(triggers)
-   console.log(Rainbin);
    return Rainbin;
 }
