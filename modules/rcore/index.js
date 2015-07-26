ab69c856e490c36c80b5a45791ad9d3f9f03e061
Rodolfo Castillo <rvallada@ucsc.edu>
2015-07-26 12:10:23 -0700

    Revert "Revert "Add reference path for VS Code""
    This reverts commit 83b2dee8f813f1ee3fdbc4e66c5733da07512f39.

--- modules/rcore/index.js
+++ modules/rcore/index.js
@@ -2,6 +2,11 @@
 
 module.exports = function(Module, done) {
   const rainCore = new Module("RainCore", "rcore");
+
+  rainCore.setConfig({
+    disableOnHookError: false
+  });
+
   rainCore.load(__dirname + '/hooks/commands');
   return done(rainCore);
 };
