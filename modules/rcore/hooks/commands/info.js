f58d857482dbe056b2cecd7b5a8dcfb707076b5c
Wolfchase <rvallada@ucsc.edu>
2015-08-07 11:18:18 -0700

    Fix command firing

--- modules/rcore/hooks/commands/info.js
+++ modules/rcore/hooks/commands/info.js
@@ -4,7 +4,7 @@ const info = {
   help: 'Information on running build (rainbot.info/userguide#info)',
   ASAP: false,
   action: function(data, respond, done) {
-    respond.say('https://github.com/Wolfchase/rainbot/releases/tag/v0.8.0');
+    respond.say('https://github.com/Wolfchase/rainbot/releases/tag/v0.8.1');
 	  return done();
   }
 };
