4f43c7df8fc8d3389174bd05eefd0edbc49a42f9
Wolfchase <rvallada@ucsc.edu>
2015-08-07 09:42:12 -0700

    Fix release info

--- modules/rcore/hooks/commands/info.js
+++ modules/rcore/hooks/commands/info.js
@@ -4,7 +4,7 @@ const info = {
   help: 'Information on running build (rainbot.info/userguide#info)',
   ASAP: false,
   action: function(data, respond, done) {
-    respond.say('https://github.com/Wolfchase/rainbot/releases/tag/v0.8.0-pre');
+    respond.say('https://github.com/Wolfchase/rainbot/releases/tag/v0.8.0');
 	  return done();
   }
 };
