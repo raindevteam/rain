cec8584cceb7adebb073bddf2b9c077a7ae72552
Ryan Printup <ryan.printup@gmail.com>
2015-11-28 15:38:17 -0500

    Initial commit

---
+++ config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"fmt"
+)
+
+type Config struct {
+	Host string
+	Port string
+	Channel string
+
+	Nick string
+	RealName string
+	UserName string
+
+	CmdPrefix string
+}
+
+func ReadConfig(string path) ($Config, err) {
+	config := Config{}
+
+	file, _ := os.Open(path)
+	decoder := json.NewDecoder(file)
+	
+	err := decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
