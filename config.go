6ecc659aaced8682eb5173a57af48ba801d13f51
Ryan Printup <ryan.printup@gmail.com>
2015-11-28 15:46:29 -0500

    Fix reference symbol mistype

--- config.go
+++ config.go
@@ -18,7 +18,7 @@ type Config struct {
 	CmdPrefix string
 }
 
-func ReadConfig(string path) ($Config, err) {
+func ReadConfig(string path) (&Config, err) {
 	config := Config{}
 
 	file, _ := os.Open(path)
