66ca3a02b9154d05d6591417ced2541f7e3330cd
wolfchase <rvallada@ucsc.edu>
2015-12-13 12:37:00 -0800

    Remove unused func

--- lib/rpc.go
+++ lib/rpc.go
@@ -8,15 +8,6 @@ import (
 	"net/rpc/jsonrpc"
 )
 
-func RpcCodecClient() rpc.ClientCodec {
-	conn, err := net.Dial("tcp", "localhost:5555")
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return jsonrpc.NewClientCodec(conn)
-}
-
 func RpcCodecClientWithPort(port string) rpc.ClientCodec {
 	conn, err := net.Dial("tcp", "localhost:"+port)
 	if err != nil {
