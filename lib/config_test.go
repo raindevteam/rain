4a1d27a9196c924ae6b4707668aea7ed01845ec6
wolfchase <rvallada@ucsc.edu>
2015-12-13 12:40:47 -0800

    Fix test again

--- lib/config_test.go
+++ lib/config_test.go
@@ -5,7 +5,7 @@ import "testing"
 //"github.com/Wolfchase/rainbot"
 
 func TestReadConfig(t *testing.T) {
-	config, err := ReadConfig("example/config.json")
+	config, err := ReadConfig("../tests/config.json")
 
 	if err != nil || config.Host != "irc.canternet.org" {
 		t.Fatalf("Couldn't read config file: ", err)
