8b8aacd2cf1844214a7d0b07ba0441753a0b9cea
Rodolfo Castillo <rvallada@ucsc.edu>
2016-02-16 01:07:49 -0800

    Fix quit; Reduce .m logic

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 
 	"github.com/RyanPrintup/nimbus"
-	"github.com/wolfchase/rainbot/lib"
+	"github.com/wolfchase/rainbot/bot"
 )
 
 func main() {
@@ -25,7 +25,7 @@ func main() {
 
 	bot := &rainbot.Bot{
 		/* Client      */ nimbus.NewClient(rainConfig.Host,
-							rainConfig.Nick, *nimConfig),
+			rainConfig.Nick, *nimConfig),
 		/* Version     */ "Alpha 0.3.0 (Steeljack)",
 		/* ModuleNames */ rainConfig.GoModules,
 		/* Channels    */ make(map[string]*rainbot.Channel),
@@ -48,8 +48,30 @@ func main() {
 
 		bot.Handler.AddInternalCommand("m", &rainbot.Command{
 			Help: "The RainBot Module Manager helps manage modules.",
-			Fun: func (msg *nimbus.Message, args []string) {
-				bot.Say(msg.Args[0], "Not exactly working yet")
+			Fun: func(msg *nimbus.Message, args []string) {
+				if len(args) == 0 {
+					bot.Say(msg.Args[0], "Error: No command specified")
+					return
+				}
+				switch args[0] {
+				case "reload":
+					if len(args) == 1 {
+						bot.Say(msg.Args[0], "Error: No module specified")
+						return
+					}
+
+					// TODO: Check for module internally
+					if bot.Handler.ModuleExists(args[1]) {
+						err := bot.ModuleReload(args[1])
+						if err != nil {
+							bot.Say(msg.Args[0], "Error: Could not reload module, view output in console")
+							return
+						}
+						bot.Say(msg.Args[0], "Reloaded module: "+args[1])
+					} else {
+						bot.Say(msg.Args[0], "Error: Module does not exist")
+					}
+				}
 			},
 			PM: true,
 			CM: true,
@@ -74,7 +96,7 @@ func main() {
 		bot.AddListener(nimbus.RPL_TOPIC, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
-			name  := msg.Args[1]
+			name := msg.Args[1]
 			topic := msg.Trailing
 
 			bot.Channels[strings.ToLower(name)].Topic = topic
@@ -87,7 +109,7 @@ func main() {
 			bot.Mu.Lock()
 
 			channel := bot.Channels[strings.ToLower(msg.Args[2])]
-			users   := strings.Split(strings.Trim(msg.Trailing, " "), " ")
+			users := strings.Split(strings.Trim(msg.Trailing, " "), " ")
 
 			for _, user := range users {
 				var name, rank string
@@ -105,12 +127,12 @@ func main() {
 		})
 
 		// Update on user Join
-		bot.AddListener(nimbus.JOIN, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.JOIN, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 			defer bot.Mu.Unlock()
 
 			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where  := msg.Args[0]
+			where := msg.Args[0]
 
 			if who == bot.Nick {
 				channel := rainbot.NewChannel(where)
@@ -123,11 +145,11 @@ func main() {
 		})
 
 		// Update on user Kick
-		bot.AddListener(nimbus.KICK, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.KICK, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
-			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where  := msg.Args[0]
+			who := msg.Args[2]
+			where := msg.Args[0]
 
 			bot.RemoveUser(who, where)
 
@@ -135,12 +157,12 @@ func main() {
 		})
 
 		// Update on user Kill
-		bot.AddListener(nimbus.KILL, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.KILL, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
 			// Implement getInfo(msg) function?
 			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where  := msg.Args[0]
+			where := msg.Args[0]
 
 			bot.RemoveUser(who, where)
 
@@ -148,11 +170,11 @@ func main() {
 		})
 
 		// Update on user part
-		bot.AddListener(nimbus.PART, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.PART, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
 			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where  := msg.Args[0]
+			where := msg.Args[0]
 
 			bot.RemoveUser(who, where)
 
@@ -160,25 +182,24 @@ func main() {
 		})
 
 		// Update on user quit
-		bot.AddListener(nimbus.QUIT, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.QUIT, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
 			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where  := msg.Args[0]
 
 			if who == bot.Nick {
-				delete(bot.Channels, strings.ToLower(where))
-				return
-				// Wait a minute...
+				// We quit
 			}
 
-			delete(bot.Channels[strings.ToLower(where)].Users, who)
+			for _, channel := range bot.Channels {
+				delete(channel.Users, who)
+			}
 
 			bot.Mu.Unlock()
 		})
 
 		// Update on nick change
-		bot.AddListener(nimbus.NICK, func (msg *nimbus.Message) {
+		bot.AddListener(nimbus.NICK, func(msg *nimbus.Message) {
 			bot.Mu.Lock()
 
 			fmt.Println(msg)
@@ -187,7 +208,7 @@ func main() {
 		})
 
 		bot.Listen()
-		result := <- bot.Quit
+		result := <-bot.Quit
 
 		fmt.Println(result)
 	})
