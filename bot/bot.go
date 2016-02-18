28d72580df194475ce6fb6306c399efcb65a75ea
Rodolfo Castillo <rvallada@ucsc.edu>
2016-02-17 23:59:51 -0800

    Comment and refactor

--- bot/bot.go
+++ bot/bot.go
@@ -5,33 +5,15 @@ import (
 	"fmt"
 	"net"
 	"net/rpc"
-	"os"
-	"os/exec"
 	"strings"
 	"sync"
 
 	"github.com/RyanPrintup/nimbus"
-	"github.com/sorcix/irc"
 )
 
-// Tickets are used to connect to a provider connection via
-// module registration.
-type Ticket struct {
-	Port string
-	Name ModuleName
-}
+/*** Bot internals ***/
 
-// A CommandRequest is used to register commands via the handler.
-type CommandRequest struct {
-	Name   CommandName
-	Module ModuleName
-}
-
-type TriggerRequest struct {
-	Name  ModuleName
-	Event Event
-}
-
+// The Channel struct is used to store information about connected channels.
 type Channel struct {
 	Name  string
 	Topic string
@@ -39,6 +21,7 @@ type Channel struct {
 	Modes string
 }
 
+// NewChannel creates a new channel.
 func NewChannel(name string) *Channel {
 	channel := &Channel{
 		Name:  name,
@@ -48,19 +31,101 @@ func NewChannel(name string) *Channel {
 	return channel
 }
 
-// The Bot struct holds the internal nimbus.Client, used to register
-// listeners for irc. ModuleNames is used to look up which plugins to start.
-// The Handler provides management of commands, listeners and triggers.
-type Bot struct {
-	*nimbus.Client
-	Version     string
-	ModuleNames map[string]string
-	Channels    map[string]*Channel
-	Parser      *Parser
-	Handler     *Handler
-	Mu          sync.Mutex
+// The Module struct holds the name of the module and its corresponding commander helper.
+type Module struct {
+	Name  string
+	Cmder *Commander
 }
 
+// NewModule creates a new module.
+func NewModule(name string, path string, cmdtype string) *Module {
+	m := &Module{
+		Name:  name,
+		Cmder: NewCommander(name, cmdtype, path),
+	}
+	return m
+}
+
+// The Bot struct holds the internal nimbus.Client, used to register listeners for irc. ModuleNames
+// is used to look up which plugins to start and are eventually passed to the handler. The Handler
+// provides management of commands, listeners and triggers. Since listeners act independently of
+// each other, a mutex is used to keep bot writes (such as channel updates) synchronised.
+type Bot struct {
+	*nimbus.Client
+	Version  string
+	Modules  map[string]*Module
+	Channels map[string]*Channel
+	Parser   *Parser
+	Handler  *Handler
+	Mu       sync.Mutex
+}
+
+/*** Module specific methods ***/
+
+// SetupModules goes through every module list found in the config and sets them up appropriately.
+func (b *Bot) SetupModules(rainConfig *Config) {
+	var name, path string
+
+	for name, path = range rainConfig.GoModules {
+		b.Modules[name] = NewModule(name, path, "go")
+	}
+}
+
+// LoadModules starts the bot's rpc master server and then calls moduleRun() on all modules.
+func (b *Bot) LoadModules() {
+	b.startRPCServer()
+	for name := range b.Modules {
+		b.moduleRun(name)
+	}
+}
+
+// ModuleReload will reload a module by telling the handler to signal a kill cleanup to the module
+// being reloaded. If the module refuses to be killed for whatever reason, reloading of the module
+// will be aborted. If the module complies, the module's corresponding process will be killed. The
+// module will then be recompiled if need be and will be restarted after.
+func (b *Bot) ModuleReload(name string) (err error) {
+	if !b.Handler.ModuleExists(name) {
+		return errors.New("Module does not exist")
+	}
+
+	c := b.Modules[name].Cmder
+
+	err = b.Handler.SignalKill(name)
+
+	if err != nil {
+		s := err.Error()
+		fmt.Println(s)
+		return errors.New("Module refused to stop, aborting reload")
+	}
+
+	err = c.Kill()
+
+	if err != nil {
+		return errors.New("Could not kill module, aborting reload")
+	}
+
+	err = c.Recompile()
+
+	if err != nil {
+		return errors.New("Could not recompile")
+	}
+
+	b.moduleRun(name)
+	return nil
+}
+
+// moduleRun starts a plugin as a provider service. This allows
+// the bot to dispatch events outbound to the module. The module
+// can then communicate back via the master rpc server.
+func (b *Bot) moduleRun(name string) {
+	c := b.Modules[name].Cmder
+	c.Start()
+}
+
+/*** IRC specific methods ***/
+
+// RemoveUser will delete a user entry from the given channel. If the user is the bot itself, the
+// bot will isntead remove the channel from the bot's channel list.
 func (b *Bot) RemoveUser(nick string, channel string) {
 	if nick == b.Nick {
 		delete(b.Channels, strings.ToLower(channel))
@@ -70,12 +135,13 @@ func (b *Bot) RemoveUser(nick string, channel string) {
 	delete(b.Channels[strings.ToLower(channel)].Users, nick)
 }
 
-// startRpcServer registers the master consumer for plugins.
-// The master consumer allows plugins to communicate with the
-// bot, allowing access to connected channels, users and registered
-// modules. Conventionally, it uses a json codec to serve.
-func (b *Bot) startRpcServer() {
-	rpc.RegisterName("Master", BotApi{b})
+/*** RPC specifc methods ***/
+
+// startRPCServer registers the master consumer for plugins. The master consumer allows plugins to
+// communicate with the bot, allowing access to connected channels, users and registered modules.
+// Conventionally, it uses a json codec to serve.
+func (b *Bot) startRPCServer() {
+	rpc.RegisterName("Master", BotAPI{b})
 	master, err := net.Listen("tcp", ":5555")
 
 	if err != nil {
@@ -91,103 +157,87 @@ func (b *Bot) startRpcServer() {
 	}()
 }
 
-// moduleRun starts a plugin as a provider service. This allows
-// the bot to dispatch events outbound to the module. The module
-// can then communicate back via the master rpc server.
-func (b *Bot) moduleRun(name string) {
-	err := exec.Command(name).Start()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+/*** Bot's master consumer API ***/
+
+// BotAPI is the exposed api served via the bot's master consumer connection
+type BotAPI struct {
+	bot *Bot
 }
 
-func (b *Bot) ModuleReload(name string) error {
-	output, err := exec.Command("go", "install", b.ModuleNames[name]+"/"+name).CombinedOutput()
-	s := string(output[:])
-	if err != nil {
-		fmt.Println(s)
-		return err
-	}
-	b.moduleRun(name)
+// A Ticket is used to connect to a provider connection via rpc.
+type Ticket struct {
+	Port string
+	Name ModuleName
+}
+
+// A CommandRequest is used to register commands via the handler.
+type CommandRequest struct {
+	Name   CommandName
+	Module ModuleName
+}
+
+// A TriggerRequest is used to register triggers via the handler.
+type TriggerRequest struct {
+	Name  ModuleName
+	Event Event
+}
+
+// Send transmits a message over irc as a PRIVMSG
+func (b BotAPI) Send(raw string, result *string) error {
+	b.bot.Send(nimbus.PRIVMSG, raw)
 	return nil
 }
 
-// It firstly starts the master consumer server for the bot.
-// It then starts every module via exec.
-func (b *Bot) LoadModules() {
-	b.startRpcServer()
-	for name, _ := range b.ModuleNames {
-		b.moduleRun(name)
-	}
-}
-
-// Api served via the bot's master consumer connection
-type BotApi struct {
-	B *Bot
-}
-
-// Sends a raw irc message via the nimbus client.
-func (bpi BotApi) Send(raw string, result *string) error {
-	bpi.B.Client.Send(irc.PRIVMSG, raw)
+// RegisterCommand registers a command from a module with the handler. The command request
+// holds the command's name and the module it belongs to (used to signal the module to fire the
+// command).
+func (b BotAPI) RegisterCommand(cr CommandRequest, result *string) error {
+	b.bot.Handler.AddCommand(cr.Name, cr.Module)
 	return nil
 }
 
-// Register a command from a module using a command request. The command request
-func (bpi BotApi) RegisterCommand(cr CommandRequest, result *string) error {
-	bpi.B.Handler.AddCommand(cr.Name, cr.Module)
-	fmt.Println(string(cr.Name) + " " + string(cr.Module))
-	return nil
-}
-
-func (bpi BotApi) RegisterTrigger(tr TriggerRequest, result *string) error {
-	listeners := bpi.B.GetListeners(string(tr.Event))
+// RegisterTrigger will register a trigger from a module with the bot handler. If this the first
+// trigger for its corresponding event, the bot will add a new listener that handles trigger firing
+// for this event.
+func (b BotAPI) RegisterTrigger(tr TriggerRequest, result *string) error {
+	listeners := b.bot.GetListeners(string(tr.Event))
 	if len(listeners) == 0 {
-		bpi.B.AddListener(string(tr.Event), func(msg *nimbus.Message) {
-			bpi.B.Handler.Fire(msg, tr.Event)
+		b.bot.AddListener(string(tr.Event), func(msg *nimbus.Message) {
+			b.bot.Handler.Fire(msg, tr.Event)
 		})
 	}
-	bpi.B.Handler.AddTrigger(tr.Name, tr.Event)
+	b.bot.Handler.AddTrigger(tr.Name, tr.Event)
 	return nil
 }
 
-func (bpi BotApi) GetVersion(mName string, result *string) error {
-	*result = bpi.B.Version
+// GetVersion will return the bot's current version.
+func (b BotAPI) GetVersion(mName string, result *string) error {
+	*result = b.bot.Version
 	return nil
 }
 
-func (bpi BotApi) GetConnectedUsers(channel string, result *map[string]string) error {
-	*result = bpi.B.Channels[strings.ToLower(channel)].Users
+// GetConnectedUsers will return a user map (where every user has a IRC rank as a value).
+func (b BotAPI) GetConnectedUsers(channel string, result *map[string]string) error {
+	*result = b.bot.Channels[strings.ToLower(channel)].Users
 	return nil
 }
 
-func (bpi BotApi) GetTopic(channel string, result *string) error {
-	if _, ok := bpi.B.Channels[strings.ToLower(channel)]; !ok {
+// GetTopic returns the channel's topic.
+func (b BotAPI) GetTopic(channel string, result *string) error {
+	if _, ok := b.bot.Channels[strings.ToLower(channel)]; !ok {
 		*result = ""
 		return errors.New("Channel doesn't exist")
 	}
 
-	*result = bpi.B.Channels[strings.ToLower(channel)].Topic
+	*result = b.bot.Channels[strings.ToLower(channel)].Topic
 	return nil
 }
 
-// Keeps a module alive
-func (bpi BotApi) Loop(n string, result *string) error {
-	c := make(chan bool)
-	func(ch chan bool) {
-
-	}(c)
-	<-c
-	return nil
-}
-
-// Registers a module with the bot. With the given port number in
-// in the Ticket, the bot creates a new rpc provider client connection
-// to the module. The name is kept in the handler for event dispatching
-// and module management.
-func (bpi BotApi) Reg(t Ticket, result *string) error {
-	fmt.Println("================== Registering: " + t.Name)
+// Register registers a module with the bot. With the given port number in the Ticket, the bot
+// creates a new rpc provider client connection to the module. The module is kept in the handler
+// for event dispatching and module management.
+func (b BotAPI) Register(t Ticket, result *string) error {
 	module := rpc.NewClientWithCodec(RpcCodecClientWithPort(t.Port))
-	bpi.B.Handler.AddModule(t.Name, module)
+	b.bot.Handler.AddModule(t.Name, module)
 	return nil
 }
