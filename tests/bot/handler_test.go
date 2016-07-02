2ec57015adc258d6b39041dce70317c1557638ed
Rodolfo Castillo Valladares <Rodolfo Castillo Valladares>
2016-07-02 12:33:08 -0700

    Rename _tests to tests

---
+++ tests/bot/handler_test.go
@@ -0,0 +1,38 @@
+package RBTbot
+
+import (
+	"testing"
+
+	"github.com/wolfchase/rainbot/bot"
+
+	"github.com/stretchr/testify/suite"
+)
+
+/****                                 Suite Configuration                                      ****/
+
+type HandlerSuite struct {
+	suite.Suite
+	h *rainbot.Handler
+}
+
+func (s *HandlerSuite) SetupTest() {
+	s.h = rainbot.NewHandler()
+}
+
+/**************************************************************************************************/
+
+/****                                      Tests Go Here                                       ****/
+
+func (s *HandlerSuite) TestAddModule() {
+
+}
+
+/**************************************************************************************************/
+
+/****                                        Run Suite                                         ****/
+
+func TestHandlerSuite(t *testing.T) {
+	suite.Run(t, new(HandlerSuite))
+}
+
+/**************************************************************************************************/
