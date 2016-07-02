7bef9890a62cb4ad3c403cfa06facdf34304b26e
Rodolfo Castillo Valladares <Rodolfo Castillo Valladares>
2016-07-02 12:36:21 -0700

    Implement new library structure

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"strings"
-	"sync"
+	"os/exec"
 
-	"github.com/wolfchase/rainbot/bot"
-	"github.com/wolfchase/rainbot/cli"
-	"github.com/wolfchase/rainbot/template"
-
-	"github.com/RyanPrintup/nimbus"
+	"github.com/urfave/cli"
 )
 
-var t = flag.String("t", "", "template type to create (mod or test)")
-var i = flag.Bool("i", false, "run the bot in interactive mode (does not connect to IRC)")
-
-func ValidLibraryOpt(opt string) bool {
+func validLibraryOption(opt string) bool {
 	switch opt {
 	case
 		"bot",
@@ -28,261 +19,70 @@ func ValidLibraryOpt(opt string) bool {
 	return false
 }
 
-func HandleFlags() {
-	if *t != "" {
-		switch *t {
-		case "test":
-			if len(flag.Args()) != 2 {
-				fmt.Println("RainBot: Invalid number of arguments")
-				fmt.Println("RainBot: Valid Args for (-t test): lib name")
-				os.Exit(1)
-			}
-
-			if !ValidLibraryOpt(flag.Args()[0]) {
-				fmt.Println("RainBot: " + flag.Args()[0] + " not a testable library")
-				os.Exit(1)
-			}
-
-			tmpl.CreateTestTemplate(flag.Args()[0], flag.Args()[1])
-		}
-		os.Exit(0)
+func validInstallOption(opt string) bool {
+	switch opt {
+	case
+		"default",
+		"simple":
+		return true
 	}
-
+	return false
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: rainbot [-t | -m] | config")
-		os.Exit(1)
-	}
+	app := cli.NewApp()
+	app.Name = "rainbot"
+	app.Usage = "Command line tool for the management of everything rainbot"
+	app.Version = "Alpha 0.4.0 (Monterey Jack)"
 
-	flag.Parse()
-	HandleFlags()
-
-	var bot *rainbot.Bot
-	var rainConfig *rainbot.Config
-	var nimConfig *nimbus.Config
-	var err error
-
-	if *i {
-		rainConfig, err = rainbot.ReadConfig(os.Args[2])
-		fmt.Println(os.Args[2])
-
-		fmt.Println(rainConfig.Host)
-
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("here?")
-			os.Exit(1)
-		}
-
-		bot = clibot.NewCLIBot(rainConfig)
-	} else {
-		nimConfig, rainConfig, err = rainbot.GetConfigs(os.Args[1])
-
-		fmt.Println(os.Args[1])
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		bot = &rainbot.Bot{
-			/* Client   */ nimbus.NewClient(rainConfig.Host, rainConfig.Nick, *nimConfig),
-			/* Version  */ "Alpha 0.4.0 (Monterey Jack)",
-			/* Modules  */ make(map[string]*rainbot.Module),
-			/* Channels */ make(map[string]*rainbot.Channel),
-			/* Parser   */ rainbot.NewParser(rainConfig.CmdPrefix),
-			/* Handler  */ rainbot.NewHandler(),
-			/* Mutex    */ sync.Mutex{},
-		}
-	}
-
-	bot.SetupModules(rainConfig)
-
-	//rlog.info("Bot", "Connecting")
-
-	fmt.Print("Connecting... ")
-
-	bot.Connect(func(e error) {
-		if e != nil {
-			fmt.Println(e)
-			return
-		}
-
-		fmt.Println("Done")
-
-		bot.LoadModules()
-
-		bot.Handler.AddInternalCommand("m", &rainbot.Command{
-			Help: "The RainBot Module Manager helps manage modules.",
-			Fun: func(msg *nimbus.Message, args []string) {
-				if len(args) == 0 {
-					bot.Say(msg.Args[0], "Error: No command specified")
-					return
-				}
-
-				switch args[0] {
-
-				case "reload":
-					if len(args) == 1 {
-						bot.Say(msg.Args[0], "Error: No module specified")
-						return
-					}
-
-					if err := bot.ModuleReload(args[1]); err != nil {
-						bot.Say(msg.Args[0], "Error: Could not reload module, view output in console")
-						return
-					}
-
-					bot.Say(msg.Args[0], "Reloaded module: "+args[1])
-
-				case "kill":
-					if len(args) == 1 {
-						bot.Say(msg.Args[0], "Error: No module specified")
-						return
-					}
-
-				}
-
+	app.Commands = []cli.Command{
+		{
+			Name: "make",
+			Aliases:   []string{"mk"},
+			Usage:     "Creates a bot",
+			UsageText: "new - Creates a new bot with given name and type. Possible types are: default, simple",
+			Action:    createNewBot,
+		},
+		{
+			Name: "template",
+			Aliases:   []string{"t", "templ"},
+			Usage:     "template <type>",
+			UsageText: "template - Create a template for either a test or module.",
+			Action: func(c *cli.Context) error {
+				return nil
 			},
-			PM: true,
-			CM: true,
-		})
+		},
+		{
+			Name: "mkconfig"
+		}
+	}
 
-		// Commands listener
-		bot.AddListener(nimbus.PRIVMSG, func(msg *nimbus.Message) {
-			if bot.Parser.IsCommand(msg.Trailing) {
-				command, args := bot.Parser.ParseCommand(msg.Trailing)
-				bot.Handler.Invoke(msg, rainbot.CommandName(command), args)
-			}
-		})
+	app.Run(os.Args)
+}
 
-		bot.AddListener(nimbus.PRIVMSG, func(msg *nimbus.Message) {
-			text := msg.Trailing
-			if text == "Hello, "+bot.GetNick() {
-				bot.Client.Say(msg.Args[0], "Hello there!")
-			}
-		})
+func createNewBot(c *cli.Context) error {
+	if !c.Args().Present() {
+		fmt.Println(c.App.ArgsUsage)
+	}
 
-		// Add/Update channel when topic is received
-		bot.AddListener(nimbus.RPL_TOPIC, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
+	if validInstallOption(c.Args().First()) {
+		if c.Args().Get(1) == "" {
+			fmt.Println("No name specified")
+			return nil
+		}
 
-			name := msg.Args[1]
-			topic := msg.Trailing
+		path := os.Getenv("GOPATH") + "/bin/" + c.Args().Get(1) + ".exe"
 
-			bot.Channels[strings.ToLower(name)].Topic = topic
+		output, err := exec.Command("go", "build", "-o", path,
+			"github.com/wolfchase/rainbot/install/"+c.Args().First()).CombinedOutput()
 
-			bot.Mu.Unlock()
-		})
+		s := string(output[:])
+		if err != nil {
+			fmt.Println("Internal command error: " + s)
+		}
+	} else {
+		fmt.Println("Not a valid install type")
+	}
 
-		// Update users for channel
-		bot.AddListener(nimbus.RPL_NAMEREPLY, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			channel := bot.Channels[strings.ToLower(msg.Args[2])]
-			users := strings.Split(strings.Trim(msg.Trailing, " "), " ")
-
-			for _, user := range users {
-				var name, rank string
-
-				if strings.ContainsAny(string(user[0]), "+ & ~ & @ & &") {
-					name, rank = user[1:], string(user[0])
-				} else {
-					name, rank = user, ""
-				}
-
-				channel.Users[name] = rank
-			}
-
-			bot.Mu.Unlock()
-		})
-
-		// Update on user Join
-		bot.AddListener(nimbus.JOIN, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-			defer bot.Mu.Unlock()
-
-			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where := msg.Args[0]
-
-			if who == bot.GetNick() {
-				channel := rainbot.NewChannel(where)
-				bot.Channels[strings.ToLower(where)] = channel
-				return
-			}
-
-			channel := bot.Channels[strings.ToLower(where)]
-			channel.Users[who] = ""
-		})
-
-		// Update on user Kick
-		bot.AddListener(nimbus.KICK, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			who := msg.Args[2]
-			where := msg.Args[0]
-
-			bot.RemoveUser(who, where)
-
-			bot.Mu.Unlock()
-		})
-
-		// Update on user Kill
-		bot.AddListener(nimbus.KILL, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			// Implement getInfo(msg) function?
-			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where := msg.Args[0]
-
-			bot.RemoveUser(who, where)
-
-			bot.Mu.Unlock()
-		})
-
-		// Update on user part
-		bot.AddListener(nimbus.PART, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-			where := msg.Args[0]
-
-			bot.RemoveUser(who, where)
-
-			bot.Mu.Unlock()
-		})
-
-		// Update on user quit
-		bot.AddListener(nimbus.QUIT, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			who, _ := bot.Parser.ParsePrefix(msg.Prefix)
-
-			if who == bot.GetNick() {
-				// We quit
-			}
-
-			for _, channel := range bot.Channels {
-				delete(channel.Users, who)
-			}
-
-			bot.Mu.Unlock()
-		})
-
-		// Update on nick change
-		bot.AddListener(nimbus.NICK, func(msg *nimbus.Message) {
-			bot.Mu.Lock()
-
-			fmt.Println(msg)
-
-			bot.Mu.Unlock()
-		})
-
-		bot.Listen()
-		result := <-bot.Quit()
-
-		fmt.Println(result)
-	})
+	return nil
 }
---
+++ setup/default.go
@@ -0,0 +1,176 @@
+package setup
+
+import (
+	"strings"
+
+	"github.com/RyanPrintup/nimbus"
+	"github.com/wolfchase/rainbot/bot"
+)
+
+type commands struct{ bot *rbot.Bot }
+
+func (c *commands) ModuleManager(msg *nimbus.Message, args []string) {
+	if len(args) == 0 {
+		c.bot.Say(msg.Args[0], "Error: No command specified")
+		return
+	}
+
+	switch args[0] {
+	case "reload":
+		if len(args) == 1 {
+			c.bot.Say(msg.Args[0], "Error: No module specified")
+			return
+		}
+
+		if err := c.bot.ModuleReload(args[1]); err != nil {
+			c.bot.Say(msg.Args[0], "Error: Could not reload module, view output in console")
+			return
+		}
+
+		c.bot.Say(msg.Args[0], "Reloaded module: "+args[1])
+	case "kill":
+		if len(args) == 1 {
+			c.bot.Say(msg.Args[0], "Error: No module specified")
+			return
+		}
+	}
+}
+
+func Default(b *rbot.Bot) {
+	c := commands{b}
+
+	b.Handler.AddInternalCommand("m", &rbot.Command{
+		Help: "The RainBot Module Manager helps manage modules.",
+		Fun:  c.ModuleManager,
+		PM:   true,
+		CM:   true,
+	})
+
+	// Commands listener
+	b.AddListener(nimbus.PRIVMSG, func(msg *nimbus.Message) {
+		if b.Parser.IsCommand(msg.Trailing) {
+			command, args := b.Parser.ParseCommand(msg.Trailing)
+			b.Handler.Invoke(msg, rbot.CommandName(command), args)
+		}
+	})
+
+	b.AddListener(nimbus.PRIVMSG, func(msg *nimbus.Message) {
+		text := msg.Trailing
+		if text == "Hello, "+b.GetNick() {
+			b.Client.Say(msg.Args[0], "Hello there!")
+		}
+	})
+
+	// Add/Update channel when topic is received
+	b.AddListener(nimbus.RPL_TOPIC, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		name := msg.Args[1]
+		topic := msg.Trailing
+
+		b.Channels[strings.ToLower(name)].Topic = topic
+
+		b.Mu.Unlock()
+	})
+
+	// Update users for channel
+	b.AddListener(nimbus.RPL_NAMEREPLY, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		channel := b.Channels[strings.ToLower(msg.Args[2])]
+		users := strings.Split(strings.Trim(msg.Trailing, " "), " ")
+
+		for _, user := range users {
+			var name, rank string
+
+			if strings.ContainsAny(string(user[0]), "+ & ~ & @ & &") {
+				name, rank = user[1:], string(user[0])
+			} else {
+				name, rank = user, ""
+			}
+
+			channel.Users[name] = rank
+		}
+
+		b.Mu.Unlock()
+	})
+
+	// Update on user Join
+	b.AddListener(nimbus.JOIN, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+		defer b.Mu.Unlock()
+
+		who, _ := b.Parser.ParsePrefix(msg.Prefix)
+		where := msg.Args[0]
+
+		if who == b.GetNick() {
+			channel := rbot.NewChannel(where)
+			b.Channels[strings.ToLower(where)] = channel
+			return
+		}
+
+		channel := b.Channels[strings.ToLower(where)]
+		channel.Users[who] = ""
+	})
+
+	// Update on user Kick
+	b.AddListener(nimbus.KICK, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		who := msg.Args[2]
+		where := msg.Args[0]
+
+		b.RemoveUser(who, where)
+
+		b.Mu.Unlock()
+	})
+
+	// Update on user Kill
+	b.AddListener(nimbus.KILL, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		// Implement getInfo(msg) function?
+		who, _ := b.Parser.ParsePrefix(msg.Prefix)
+		where := msg.Args[0]
+
+		b.RemoveUser(who, where)
+
+		b.Mu.Unlock()
+	})
+
+	// Update on user part
+	b.AddListener(nimbus.PART, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		who, _ := b.Parser.ParsePrefix(msg.Prefix)
+		where := msg.Args[0]
+
+		b.RemoveUser(who, where)
+
+		b.Mu.Unlock()
+	})
+
+	// Update on user quit
+	b.AddListener(nimbus.QUIT, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		who, _ := b.Parser.ParsePrefix(msg.Prefix)
+
+		if who == b.GetNick() {
+			// We quit
+		}
+
+		for _, channel := range b.Channels {
+			delete(channel.Users, who)
+		}
+
+		b.Mu.Unlock()
+	})
+
+	// Update on nick change
+	b.AddListener(nimbus.NICK, func(msg *nimbus.Message) {
+		b.Mu.Lock()
+
+		b.Mu.Unlock()
+	})
+}
