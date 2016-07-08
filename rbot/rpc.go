cd3ffd2d75cda8832afd2a1599731c9ced812497
Rodolfo Castillo <rvallada@ucsc.edu>
2016-07-08 16:59:32 -0700

    Finish rpc_test.go

--- rbot/rpc.go
+++ rbot/rpc.go
@@ -1,20 +1,18 @@
 package rbot
 
 import (
-	"fmt"
 	"io"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
-func RpcCodecClientWithPort(port string) rpc.ClientCodec {
+func RpcCodecClientWithPort(port string) (rpc.ClientCodec, error) {
 	conn, err := net.Dial("tcp", "localhost:"+port)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
-	return jsonrpc.NewClientCodec(conn)
+	return jsonrpc.NewClientCodec(conn), nil
 }
 
 func RpcCodecServer(conn io.ReadWriteCloser) rpc.ServerCodec {
--- rbot/rpc_test.go
+++ rbot/rpc_test.go
@@ -1,81 +1,121 @@
 package rbot
 
-/*
-/****                                 Suite Configuration                                      ****
+import (
+	"errors"
+	"net"
+	"net/rpc"
+	"testing"
+)
 
-type RpcSuite struct {
-	suite.Suite
-	RpcServer  net.Listener
-	RpcClient  *rpc.Client
-	TestPort   string
-	GotMessage bool
+type RPCHandler struct {
+	RPCServer net.Listener
+	conn      rpc.ServerCodec
+	done      chan bool
+	TestPort  string
+	stop      bool
+	GotRPC    bool
 }
 
-func (s *RpcSuite) SetupTest() {
-	s.GotMessage = false
-	s.TestPort = "5556"
+func (r *RPCHandler) SetupTest() {
+	r.stop = false
+	r.GotRPC = false
+	r.done = make(chan bool)
+	r.TestPort = "5556"
+}
+
+// TODO: Create separate function to handle erroring
+func (r *RPCHandler) CreateRPCServer() error {
+	srv := rpc.NewServer()
+	srv.RegisterName("TestMaster", TestAPI{r})
+
+	var err error
+	r.RPCServer, err = net.Listen("tcp", ":"+r.TestPort)
+
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			conn, err := r.RPCServer.Accept()
+			if err != nil || r.stop {
+				r.done <- true
+				return
+			}
+			r.conn = RpcCodecServer(conn)
+			srv.ServeCodec(r.conn)
+		}
+	}()
+	return nil
+}
+
+func (r *RPCHandler) CloseRPCServer() error {
+	r.stop = true
+	err := r.RPCServer.Close()
+	<-r.done
+	return err
 }
 
 type TestAPI struct {
-	s *RpcSuite
+	t *RPCHandler
 }
 
-func (r TestAPI) Send(msg string, result *string) error {
+func (tapi TestAPI) Send(msg string, result *string) error {
 	if msg == "Got RPC?" {
-		r.s.GotMessage = true
+		tapi.t.GotRPC = true
 		return nil
 	}
 	return errors.New("Didn't receive right message")
 }
 
-/**************************************************************************************************
+// Check if we can create and close an RPC server successfully using the RPC server codec.
+func TestRpcCodecServer(t *testing.T) {
+	r := RPCHandler{}
+	r.SetupTest()
 
-/****                                      Tests Go Here                                       ****
-
-func (s *RpcSuite) Test1RpcCodecServerInit() {
-	rpc.RegisterName("TestMaster", TestAPI{s})
-
-	var err error
-	s.RpcServer, err = net.Listen("tcp", ":"+s.TestPort)
-
+	err := r.CreateRPCServer()
 	if err != nil {
-		s.FailNow("Failed to create RPC server")
+		t.Fatalf("Could not create rpc server! %s:", err.Error())
 	}
 
-	go func() {
-		for {
-			conn, err := s.RpcServer.Accept()
-			if err != nil {
-				break
-			}
-			go rpc.ServeCodec(rbot.RpcCodecServer(conn))
-		}
-	}()
+	err = r.CloseRPCServer()
+	if err != nil {
+		t.Fatalf("Could not close RPC server! %s:", err.Error())
+	}
 }
 
-func (s *RpcSuite) Test2RpcCodecClientConnect() {
-	s.RpcClient = rpc.NewClientWithCodec(rbot.RpcCodecClientWithPort(s.TestPort))
-	s.NotNil(s.RpcClient, "Client Created")
+// Check if we can create a client without erroring.
+func TestRpcCodecClientWithPort(t *testing.T) {
+	r := RPCHandler{}
+	r.SetupTest()
+	r.CreateRPCServer()
+	defer r.CloseRPCServer()
+
+	RPCClient, err := RpcCodecClientWithPort(r.TestPort)
+	defer RPCClient.Close()
+	if err != nil {
+		t.Fatalf("Could not create an RPC client! %s:", err.Error())
+	}
 }
 
-func (s *RpcSuite) Test3RpcCodecServerClientComm() {
+// Let's double check and make sure our server and client can speak to each other
+func TestRpcCodecServerClientComm(t *testing.T) {
+	r := RPCHandler{}
+	r.SetupTest()
+	r.CreateRPCServer()
+	defer r.CloseRPCServer()
+
+	RPCCodec, _ := RpcCodecClientWithPort(r.TestPort)
+	RPCClient := rpc.NewClientWithCodec(RPCCodec)
+	defer RPCClient.Close()
+
 	var result string
-	err := s.RpcClient.Call("TestMaster.Send", "Got RPC?", &result)
-	s.Nil(err)
-	s.True(s.GotMessage, "Communication between rpc server and client successful")
+	err := RPCClient.Call("TestMaster.Send", "Got RPC?", &result)
+	if err != nil {
+		t.Fatalf("Error while trying to send RPC message: %s", err.Error())
+	}
+
+	if !r.GotRPC {
+		t.Fatalf("Could not send correct message over RPC")
+	}
 }
-
-func (s *RpcSuite) TearDownSuite() {
-	s.RpcServer.Close()
-}
-
-/**************************************************************************************************
-
-/****                                        Run Suite                                         ****
-
-func TestRpcSuite(t *testing.T) {
-	suite.Run(t, new(RpcSuite))
-}
-
-/**************************************************************************************************
-*/
