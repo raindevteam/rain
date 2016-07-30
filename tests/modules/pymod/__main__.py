c8c2968c48202799833e0bee0e75cf9a9788f846
Rodolfo Castillo Valladares <=>
2016-07-30 15:23:16 -0700

    Update license to AGPL

--- tests/modules/pymod/__main__.py
+++ tests/modules/pymod/__main__.py
@@ -1,18 +1,21 @@
-from pyrml import Module
+# Copyright (C) 2015  Rodolfo Castillo-Valladares & Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Send any inquiries you may have about this program to: rcvallada@gmail.com
 
-class Pymod(Module):
-    def __init__(self):
-        Module.__init__(self, "Pymod", "Test module")
+import pyrml
 
-    def echo(self, msg, args):
-        self.say(msg["Args"][0], " ".join(args))
+m = pyrml.Module("Pymod", "Test Module")
 
-if __name__ == '__main__':
-    m = Pymod()
+@m.command("echo")
+def echo(msg, args):
+    m.say(msg["Args"][0], " ".join(args))
 
-    m.add_command("echo", {
-        "Help": "Repeats arguments",
-        "Fun": m.echo
-    })
-
-    m.register()
+m.register()
