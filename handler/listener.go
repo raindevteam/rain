69c9aeb11dc89207a7fd7f8e4b8f05a853ed9dd7
Rodolfo Castillo-Valladares <rcvallada@gmail.com>
2017-05-23 01:27:57 -0700

    Refactor testing

--- handler/listener.go
+++ handler/listener.go
@@ -1,8 +1,10 @@
 // Copyright 2015-2017, Rodolfo Castillo-Valladares. All rights reserved.
+//
 // This file is governed by the Modified BSD License. You should have
 // received a copy of the license (LICENSE.md) with this file's program.
+// You may find the program here: https://github.com/raindevteam/rain
 //
-// Contact Rodolfo at rcvallada@gmail.com for any inquires of this file.
+// Contact Rodolfo at rcvallada@gmail.com for any inquiries of this file.
 
 package handler
 
